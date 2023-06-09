e56a20aa5d1cf6cbb6f4bdf8a7dab401e6ebaf94
kdhillondev <karanvirdhillon499+developer@Gmail.com>
2023-06-09 15:25:44 -0700

    challenge 1

--- Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,21 @@
 public class YourInitials {
     public static void main(String[] args) {
-         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+        System.out.print("k   k  "); 
+        System.out.println("  DDDD"); 
+        System.out.print("k  k   "); 
+        System.out.println("  D   D"); 
+        System.out.print("k k    "); 
+        System.out.println("  D   D"); 
+        System.out.print("kk     "); 
+        System.out.println("  D   D"); 
+        System.out.print("k k    "); 
+        System.out.println("  D   D"); 
+        System.out.print("k  k   "); 
+        System.out.println("  D   D"); 
+        System.out.print("k   k  "); 
+        System.out.print("  DDDD"); 
+
+        
+        // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
     }
 }
