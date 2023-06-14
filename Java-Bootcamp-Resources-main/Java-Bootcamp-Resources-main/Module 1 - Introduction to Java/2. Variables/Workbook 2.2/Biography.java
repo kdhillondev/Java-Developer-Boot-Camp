2ad8a3f2a266b0b87748004e668e10d861bdbfed
kdhillondev <karanvirdhillon499+developer@Gmail.com>
2023-06-13 19:32:14 -0700

    challenge 2.2

--- Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
+++ Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
@@ -1,3 +1,5 @@
+import java.util.jar.Attributes.Name;
+
 public class Biography {
     public static void main(String[] args) {
         
@@ -5,18 +7,26 @@ public class Biography {
 
 
         // name (text)
+        String name="Karn";
         // age (whole number)
+        int age=29;
         // country (text)
+        String country="Canada";
         // sport (text)
+        String sport="Soccer";
         // hours (whole number)
+        int hours =1;
         // game (text)
+        String game="PS4";
         // subject (text)
+        String subject="ICS";
         // grade (character)
+        char grade='A';
      
-        System.out.println("My name is <name>. I'm <age> years old, and I'm from <country> ");
-        System.out.println("My favourite sport is <sport>. I play for <hours> hours a day");
-        System.out.println("When I'm tired, I like to play <game>.");
-        System.out.println("In school, my favourite subject was <subject>, I scored a <grade>.");
+        System.out.println("My name is "+name+". I'm "+age+" years old, and I'm from "+country);
+        System.out.println("My favourite sport is"+ sport+". I play for "+hours+" hours a day");
+        System.out.println("When I'm tired, I like to play "+game+".");
+        System.out.println("In school, my favourite subject was " +subject+", I scored a "+grade+".");
 
         // Compare your result to what's on Learn the Part.
     }
