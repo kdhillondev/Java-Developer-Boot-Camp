7b25e6df5758db52c2121d6f51fcd4796bad9468
kdhillondev <karanvirdhillon499+developer@Gmail.com>
2023-06-13 18:49:18 -0700

    Grades

---
+++ SectionTwo/Grades.java
@@ -0,0 +1,13 @@
+public class Grades {
+    public static void main (String[] args){
+        char potions,arithmacy,charms;
+        potions='A';
+        arithmacy='B';
+        charms='C';
+
+        System.out.println("Potions "+potions);
+        System.out.println("Arithmacy "+arithmacy);
+        System.out.println("charms "+charms);
+        
+    }
+}
