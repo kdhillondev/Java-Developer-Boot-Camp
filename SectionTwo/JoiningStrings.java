0fdd247d8a4e982383b1f3feac6eeb2b97147a88
kdhillondev <karanvirdhillon499+developer@Gmail.com>
2023-06-13 18:37:58 -0700

    Joining Strings

---
+++ SectionTwo/JoiningStrings.java
@@ -0,0 +1,9 @@
+public class JoiningStrings {
+    public static void main(String[] args){
+        int year=2023;
+        String winner="Spain";
+
+        String announcement="The winenrs of the "+year+" world cup are:"+winner;
+        System.out.println(announcement);
+    }
+}
