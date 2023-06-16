5fe3f401bee61637ddfc3ebb7322fd983d1eb761
kdhillondev <karanvirdhillon499+developer@Gmail.com>
2023-06-15 20:29:08 -0700

    whole numbers

---
+++ SectionTwo/WholeNumbers.java
@@ -0,0 +1,9 @@
+public class WholeNumbers {
+    public static void main(String[]args){
+long globalPopulation=8000000000L;
+System.out.println("the global population is "+globalPopulation);
+long dailyGoogleSearches=850000000L;
+System.out.println("Daily, there are "+dailyGoogleSearches+" google searches");
+
+    }
+}
