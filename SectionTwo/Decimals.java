3e4b5b20731968dedb36bc52fc704c47264a105f
kdhillondev <karanvirdhillon499+developer@Gmail.com>
2023-06-15 20:37:53 -0700

    Decimals

---
+++ SectionTwo/Decimals.java
@@ -0,0 +1,12 @@
+public class Decimals {
+    public static void main(String[] args){
+        double percentage=73.5;
+        System.out.println(percentage+" of percentages are made up");
+
+        //always use double for math calculations
+        double wallet = 20;
+        int people = 3;
+        System.out.println(wallet/people);
+
+    }
+}
