7d4a2c9e23c3c7830a627843a8fec961c3e9e173
kdhillondev <karanvirdhillon499+developer@Gmail.com>
2023-06-22 22:35:09 -0700

    updates

--- SectionThree/IfElseIfElse.java
+++ SectionThree/IfElseIfElse.java
@@ -2,12 +2,32 @@ public class IfElseIfElse {
     public static void main(String[] args) {
 
         String weather = "rainy";
+        if(weather.equals("sunny")){
+            System.out.println("It's a nice day for a walk.");
+        } else if (weather.equals("rainy")){
+             System.out.println("Bring an umbrella!");
+        }else {
+            System.out.println("Not sure what to do in this weather.");
+        }
 
         // sunny: System.out.println("It's a nice day for a walk.");
         // rainy: System.out.println("Bring an umbrella!");
         // otherwise: System.out.println("Not sure what to do in this weather.");
 
         int score = 85;
+        if(score>=80){
+            System.out.println("You got an A!");
+        } else if(score>=70){
+             System.out.println("You got a B!");
+        } else if(score>=60){
+            System.out.println("You got a C.");
+        }else if(score>=50) {
+ System.out.println("You got a D.");
+        }
+        else{
+            System.out.println("You failed :(");
+
+        }
 
         // 80 and above: System.out.println("You got an A!");
         // 70 and above: System.out.println("You got a B!");
