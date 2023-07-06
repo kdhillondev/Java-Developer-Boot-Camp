bce3e4f69f641ffd478bf16db2cdd7daf9b41726
kdhillondev <karanvirdhillon499+developer@Gmail.com>
2023-07-05 19:14:41 -0700

    section 4 code

---
+++ SectionFour/Parameters.java
@@ -0,0 +1,13 @@
+public class Parameters {
+    
+    public static void main(String[] args){
+
+//values passing in are called arguments
+calculateArea(2.3,3.6);
+       
+    }
+//variables that will end up storing these values are called paremeters
+     public static void calculateArea (double length, double width){
+System.out.println("Area:"+length+" x"+width+" is "+(length*width));
+        }
+}
