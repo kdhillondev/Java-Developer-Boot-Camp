eb265382262f802c660b62190baf90196980d2f5
kdhillondev <karanvirdhillon499+developer@Gmail.com>
2023-07-18 19:58:02 -0700

    doc workbook

---
+++ SectionFour/DocComments.java
@@ -0,0 +1,45 @@
+public class DocComments{
+    public static void main(String[] args){
+
+
+    }
+    /*
+     * Function name: greet
+     * 
+     * Inside the function:
+     *  1. prints: 'Hi'
+     */
+    public static void greet(){
+        System.out.println("Hi");
+    }
+/**
+ * Function name: printText
+ * @param name (String)
+ * @param age (String)
+ * 
+ * Inside the function:
+ *  1. prints the name and age as part of a text
+ */
+    public static void printText(String name, String age){
+        System.out.println("Hi, I am "+name+ " I am "+age+" years old.");
+    }
+/** press ** 
+ * Java populates the function format
+ * All you have to do is pass the function name
+ * Give a small description
+ * 
+ * Function name: calculateArea
+ * 
+ * @param length (double)
+ * @param width (double)
+ * @return (double)
+ * 
+ * Inside the function:
+ *  1. this function calculates the area and returns it.
+ * 
+ */
+    public static double calculateArea(double length, double width){
+        double area= length*width;
+        return area;
+    }
+}
