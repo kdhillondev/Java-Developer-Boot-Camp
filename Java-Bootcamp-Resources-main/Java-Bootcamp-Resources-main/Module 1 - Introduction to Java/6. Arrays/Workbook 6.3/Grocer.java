cd3075c4536c20d17f79d097b5bb8fe8d45bcc97
kdhillondev <karanvirdhillon499+developer@Gmail.com>
2023-09-26 17:08:49 -0700

    workbook 6.3

--- Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.3/Grocer.java
+++ Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.3/Grocer.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Grocer {
     public static void main(String[] args) {
         
+        String[] storeItems={"apples", "bananas", "candy", "chocolate", "coffee", "tea"};
         // Instructions for this workbook are on Learn the Part (Workbook 6.3).
 
-        System.out.println("\nDo you sell coffee?");
-        System.out.println("\nWe have that in aisle: <index>");
+        System.out.println("\nDo you sell:");
+        Scanner scan= new Scanner(System.in);
+        String input=scan.nextLine();
+
+        int count=0;
+        boolean foundItem=false;
+        for(String item: storeItems){
+            if (input.equalsIgnoreCase(input)){
+                 System.out.println("\nWe have that in aisle: "+count);
+                 foundItem=true;
+            }
+            count++;
+        }
+
+        if(!foundItem){
+            System.out.println("sorry item is not for sale");
+        }
     
     }
 }
