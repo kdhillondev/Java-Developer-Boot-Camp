2f7d5a269b090834428605fae45ff73005703cf1
kdhillondev <karanvirdhillon499+developer@Gmail.com>
2023-09-26 17:03:30 -0700

    updates

--- Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Challenge/Blackjack.java
+++ Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Challenge/Blackjack.java
@@ -9,17 +9,34 @@ public class Blackjack {
         System.out.println("Do you have a knack for Black Jack?");
         System.out.println("We shall see..");
         System.out.println("..Ready? Press anything to begin!");
+
         //Task 3 – Wait for the user to press enter.
+        Scanner scan = new Scanner(System.in);
+        String userInput=scan.nextLine();
         //Task 4 – Get two random cards.
         //       – Print them: \n You get a \n" + <randomCard> + "\n and a \n" + <randomCard>
+        int newCard=drawRandomCard();
+        int userHand=drawRandomCard();
+        System.out.println(" \\n" + //
+                " You get a \\n" + //
+                "\"" + cardString(userHand) +"\"\\n" + //
+                " and a \\n" + //
+                "\"" + newCard);
+        userHand+=newCard;
+        
+
+
+
 
         //Task 5 – Print the sum of your hand value.
         //       – print: your total is: <hand value>
 
-        
+        System.out.println("your total is: "+userHand);
         //Task 6 – Get two random cards for the dealer.
         //       – Print: The dealer shows \n" + <first card> + "\nand has a card facing down \n" + <facedown card>
         //       – Print: \nThe dealer's total is hidden
+
+        
 
        
         //Task 8 – Keep asking the player to hit or stay (while loop).
@@ -44,7 +61,12 @@ public class Blackjack {
      * Inside the function:
      *   1. Gets a random number between 1 and 13.
      *   2. Returns a card.
-     */
+     */ 
+
+    public static int drawRandomCard(){
+        int randomNum=(int) (Math.random()*13)+1;
+        return randomNum;
+    }
 
     /** Task 2 – make a function that returns a String drawing of the card.
      * Function name – cardString
@@ -54,6 +76,120 @@ public class Blackjack {
      * Inside the function:
      *   1. Returns a String drawing of the card.
      */
+    public static String cardString(int cardNumber){
+        String drawing="";
+        switch(cardNumber){
+            case 1: 
+                drawing= "   _____\n"+
+                   "  |A _  |\n"+ 
+                   "  | ( ) |\n"+
+                   "  |(_'_)|\n"+
+                   "  |  |  |\n"+
+                   "  |____V|\n";
+                break;
+            case 2: 
+                drawing= "   _____\n"+              
+                    "  |2    |\n"+ 
+                    "  |  o  |\n"+
+                    "  |     |\n"+
+                    "  |  o  |\n"+
+                    "  |____Z|\n";
+                break;
+            case 3: 
+                drawing=   "   _____\n" +
+                  "  |3    |\n"+
+                  "  | o o |\n"+
+                  "  |     |\n"+
+                  "  |  o  |\n"+
+                  "  |____E|\n";
+                break;
+                case 4: 
+                drawing=     "   _____\n" +
+                   "  |4    |\n"+
+                   "  | o o |\n"+
+                   "  |     |\n"+
+                   "  | o o |\n"+
+                   "  |____h|\n";
+
+                break;
+            case 5: 
+                drawing=     "   _____ \n" +
+                    "  |5    |\n" +
+                    "  | o o |\n" +
+                    "  |  o  |\n" +
+                    "  | o o |\n" +
+                    "  |____S|\n";
+                break;
+            case 6: 
+                drawing= "   _____ \n" +
+                    "  |6    |\n" +
+                    "  | o o |\n" +
+                    "  | o o |\n" +
+                    "  | o o |\n" +
+                    "  |____6|\n";
+            case 7: 
+                drawing= "   _____ \n" +
+                    "  |7    |\n" +
+                    "  | o o |\n" +
+                    "  |o o o|\n" +
+                    "  | o o |\n" +
+                    "  |____7|\n";
+                break;
+
+            case 8: 
+                drawing=    "   _____ \n" +
+                    "  |8    |\n" +
+                    "  |o o o|\n" +
+                    "  | o o |\n" +
+                    "  |o o o|\n" +
+                    "  |____8|\n";
+                break;
+            case 9: 
+                drawing=  "   _____ \n" +
+                    "  |9    |\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |____9|\n";
+                break;
+            case 10: 
+                drawing= "   _____ \n" +
+                    "  |10  o|\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |___10|\n";
+                break;
+            case 11: 
+                drawing= "   _____\n" +
+                    "  |J  ww|\n"+ 
+                    "  | o {)|\n"+ 
+                    "  |o o% |\n"+ 
+                    "  | | % |\n"+ 
+                    "  |__%%[|\n";
+                break;
+            case 12: 
+                drawing= "   _____\n" +
+                    "  |Q  ww|\n"+ 
+                    "  | o {(|\n"+ 
+                    "  |o o%%|\n"+ 
+                    "  | |%%%|\n"+ 
+                    "  |_%%%O|\n";
+                break;
+            case 13: 
+                drawing= "   _____\n" +
+                    "  |K  WW|\n"+ 
+                    "  | o {)|\n"+ 
+                    "  |o o%%|\n"+ 
+                    "  | |%%%|\n"+ 
+                    "  |_%%%>|\n";
+                break;
+            default: break;
+        }
+        return drawing;
+
+        
+    }
 
     public static String faceDown() {
         return
--- Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Pokerito/Pokerito.java
+++ Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Pokerito/Pokerito.java
@@ -6,6 +6,7 @@ public class Pokerito {
 
         /*Task 2: Explain the rules
 
+
         >>Let's play Pokerito. Type anything when you're ready.
         |
         >>It's like Poker, but a lot simpler.
@@ -19,7 +20,15 @@ public class Pokerito {
         >> • Ready? Type anything if you are.
         |
         */
-        
+        System.out.println(">Let's play Pokerito. Type anything when you're ready.\n");
+        System.out.println("It's like Poker, but a lot simpler.");
+        System.out.println("There are two players, you and the computer.");
+        System.out.println("The dealer will give each player one card.");
+        System.out.println("The player with the most river matches wins!");
+        System.out.println("If the matches are equal, everyone's a winner!\n");
+        System.out.println("Ready? Type anything if you are.");
+
+        String userInput=scan.nextLine();
 
         /*Task 3: Present the user with a card
          println 'Here's your card:'
@@ -28,6 +37,12 @@ public class Pokerito {
          println 'Here's the computer's card:'
          <show computer's card>
         */
+        System.out.println("'Here's your card:'");
+        String userCard=randomCard();
+        System.out.println(userCard+"\n");
+        String cpuCard=randomCard();
+        System.out.println(cpuCard+"\n");
+
 
         int yourMatches = 0;
         int computerMatches =0;
@@ -45,6 +60,35 @@ public class Pokerito {
          *      <print card>
          *      ...
          */
+        System.out.println("Now, the dealer will draw five cards. Press enter to continue.");
+        String card1 = randomCard();
+        //String card1=randomCard();
+        String card2=randomCard();
+        String card3=randomCard();
+        String card4=randomCard();
+        String card5=randomCard();
+
+        int count=0;
+        while(count<5){
+            userInput=scan.nextLine();
+            if(userInput.equals("")) count++;
+            else{
+                continue;
+            }
+            switch(count){
+                case 1: System.out.println(card1); break;
+                case 2: System.out.println(card2); break;
+                case 3: System.out.println(card3); break;
+                case 4: System.out.println(card4);break;
+                case 5:System.out.println(card5);break;
+                default:break;
+            }
+        }
+
+       
+
+        
+        
 
         /** Task 5 - Get the winner
          * 
@@ -58,6 +102,31 @@ public class Pokerito {
          * • If the matches are equal, print: everyone wins!.
          */
 
+         int userScore=0;
+         int cpuScore=0;
+         
+         if(userCard.equals(card1)) userScore++;
+         if (userCard.equals(card2)) userScore++;
+         if (userCard.equals(card3)) userScore++;
+         if (userCard.equals(card4)) userScore++;
+         if (userCard.equals(card5)) userScore++;
+
+         if(cpuCard.equals(card1)) cpuScore++;
+         if(cpuCard.equals(card2)) cpuScore++;
+         if (cpuCard.equals(card3)) cpuScore++;
+         if(cpuCard.equals(card4)) cpuScore++;
+         if (cpuCard.equals(card5)) cpuScore++;
+
+       
+         if(userScore>cpuScore){
+            System.out.println("user wins");
+         }else if (cpuScore>userScore){
+            System.out.println("cpu wins");
+         }else{
+            System.out.println("Tie!");
+         }
+
+         
          scan.close();
     }
 
@@ -70,5 +139,121 @@ public class Pokerito {
      *   1. Gets a random number between 1 and 13.
      *   2. Returns a card that matches the random number (get the String values from cards.text).   
      */
+
+     public static String randomCard(){
+
+        int random =(int) (Math.random()*13+1);
+        String card="";
+
+        switch(random){
+            case 1: card= "   _____\n"+
+                   "  |A _  |\n"+ 
+                   "  | ( ) |\n"+
+                   "  |(_'_)|\n"+
+                   "  |  |  |\n"+
+                   "  |____V|\n";
+                   break;
+
+            case 2: card=  "   _____\n"+              
+                    "  |2    |\n"+ 
+                    "  |  o  |\n"+
+                    "  |     |\n"+
+                    "  |  o  |\n"+
+                    "  |____Z|\n";
+                  break;
+
+            case 3: card="   _____\n" +
+                  "  |3    |\n"+
+                  "  | o o |\n"+
+                  "  |     |\n"+
+                  "  |  o  |\n"+
+                  "  |____E|\n";
+                  break;
+
+            case 4: card="   _____\n" +
+                   "  |4    |\n"+
+                   "  | o o |\n"+
+                   "  |     |\n"+
+                   "  | o o |\n"+
+                   "  |____h|\n";
+                   break;
+
+            case 5: card="   _____ \n" +
+                    "  |5    |\n" +
+                    "  | o o |\n" +
+                    "  |  o  |\n" +
+                    "  | o o |\n" +
+                    "  |____S|\n";
+                    break;
+
+            case 6: card="   _____ \n" +
+                    "  |6    |\n" +
+                    "  | o o |\n" +
+                    "  | o o |\n" +
+                    "  | o o |\n" +
+                    "  |____6|\n";
+                    break;
+
+            case 7: card= "   _____ \n" +
+                    "  |7    |\n" +
+                    "  | o o |\n" +
+                    "  |o o o|\n" +
+                    "  | o o |\n" +
+                    "  |____7|\n";
+                    break;
+
+            case 8: card= "   _____ \n" +
+                    "  |8    |\n" +
+                    "  |o o o|\n" +
+                    "  | o o |\n" +
+                    "  |o o o|\n" +
+                    "  |____8|\n";
+                    break;
+
+            case 9: card= "   _____ \n" +
+                    "  |9    |\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |____9|\n";
+                    break;
+
+            case 10: card="   _____ \n" +
+                    "  |10  o|\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |___10|\n";
+                    break;
+
+            case 11: card= "   _____\n" +
+                    "  |J  ww|\n"+ 
+                    "  | o {)|\n"+ 
+                    "  |o o% |\n"+ 
+                    "  | | % |\n"+ 
+                    "  |__%%[|\n";
+                    break;
+
+            case 12: card=  "   _____\n" +
+                    "  |Q  ww|\n"+ 
+                    "  | o {(|\n"+ 
+                    "  |o o%%|\n"+ 
+                    "  | |%%%|\n"+ 
+                    "  |_%%%O|\n";
+                    break;
+
+            case 13: card="   _____\n" +
+                    "  |K  WW|\n"+ 
+                    "  | o {)|\n"+ 
+                    "  |o o%%|\n"+ 
+                    "  | |%%%|\n"+ 
+                    "  |_%%%>|\n";
+                    break;
+
+            default: break;
+        }
+        return card;
+
+     }
 
 }
