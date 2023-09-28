f2181ae7e92d7a70f27242f7da468688307d6405
kdhillondev <karanvirdhillon499+developer@Gmail.com>
2023-09-27 20:25:48 -0700

    HighScores.java

--- Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.5/HighScore.java
+++ Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.5/HighScore.java
@@ -5,10 +5,24 @@ public class HighScore {
         
         // Instructions for this workbook are on Learn the Part (Workbook 6.5).
 
-        System.out.print("Here are the scores: <score elements>");
+                int[] numberArray={randomNumber(),randomNumber(),randomNumber(),randomNumber(),randomNumber()};
+
+System.out.print("Here are the scores: ");
+
+                for (int i=0;i<numberArray.length;i++){
+                    System.out.print(numberArray[i]+" ");
+                    if(highScore<numberArray[i]){
+                        highScore=numberArray[i];
+                    }
+                }
 
         System.out.println("\n\nThe highest score is: " + highScore + ". Give that man a cookie!");
+
         
     }    
+
+    public static int randomNumber(){
+        return (int) Math.random()*4999;
+    }
 
 }
