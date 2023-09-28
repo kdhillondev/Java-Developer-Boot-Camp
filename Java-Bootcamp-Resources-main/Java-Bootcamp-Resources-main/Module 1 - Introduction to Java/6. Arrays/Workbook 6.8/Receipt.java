296b807ba66638c3f1f2eb2ba17002054885ff3c
kdhillondev <karanvirdhillon499+developer@Gmail.com>
2023-09-27 21:24:03 -0700

    prices

--- Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.8/Receipt.java
+++ Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.8/Receipt.java
@@ -2,9 +2,16 @@ public class Receipt {
     public static void main(String[] args) {
 
 
+        String[] stores={"Gala", "Granny Smith", "Macintosh"};
+        double[] prices={1.99, 1.49, 1.29};
+
         System.out.println("Here's your receipt:\n");
         
         // See instructions on Learn the Part (Workbook 6.8)
+
+        for(int i=0;i<stores.length;i++){
+            System.out.println(stores+":"+"$price "+prices[i]);
+        }
         
         System.out.println("\t<apple i >: $<price i >"); // to be used in for loop.
     }
