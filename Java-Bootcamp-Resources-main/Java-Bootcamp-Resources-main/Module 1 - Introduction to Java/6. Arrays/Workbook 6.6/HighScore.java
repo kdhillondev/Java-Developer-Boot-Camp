64693ebdb73991b153c3f5ee239d91887c6e1c59
kdhillondev <karanvirdhillon499+developer@Gmail.com>
2023-09-27 20:30:34 -0700

    6.6

--- Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.6/HighScore.java
+++ Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.6/HighScore.java
@@ -1,13 +1,14 @@
 public class HighScore {
     public static void main(String[] args) {
         int[] scores = {randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber()};
-        
+        int pos=-1;
         int highScore = 0;
         System.out.print("Here are the scores: ");    
         for (int i = 0; i < scores.length; i++) {
             System.out.print(scores[i] + " ");
             if (scores[i] > highScore) {
                 highScore = scores[i];
+                pos=i;
             }
         }
         
@@ -15,7 +16,7 @@ public class HighScore {
         System.out.println("\n\nThe highest score is: " + highScore + ". Impressive!");
         
         // TODO : See instructions on Learn the Part (Workbook 6.6)
-        System.out.println("It's the gentleman in seat: <index>. Give that man a cookie!");
+        System.out.println("It's the gentleman in seat: "+pos+". Give that man a cookie!");
 
     }
     
