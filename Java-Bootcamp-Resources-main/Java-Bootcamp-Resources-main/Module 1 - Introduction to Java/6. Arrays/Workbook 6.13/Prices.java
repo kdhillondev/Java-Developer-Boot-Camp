9a33467163cd8cb19cdef115051cf0515eed97a6
kdhillondev <karanvirdhillon499+developer@Gmail.com>
2023-10-11 18:05:48 -0700

    updated

--- Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.13/Prices.java
+++ Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.13/Prices.java
@@ -1,5 +1,39 @@
 public class Prices {
     public static void main(String[] args) {
         // The instructions for this workbook are on Learn the Part (workbook 6.13)
+
+        double[][] example={
+            {12.99,8.99,9.99,10.49,11.99},
+            {0.99,1.99,2.49,1.49,2.99},
+            {8.99,7.99,9.49,9.99,10.99}
+        };
+
+        //for the sake of the excercise we will index another array the long way
+
+        double[][] prices= new double[3][5];
+        prices[0][0]=12.99;
+        prices[0][1]=8.99;
+        prices[0][2]=9.99;
+        prices[0][3]=10.49;
+        prices[0][4]=11.99;
+
+        prices[1][0]=0.99;
+        prices[1][1]=1.99;
+        prices[1][2]=2.49;
+        prices[1][3]=1.49;
+        prices[1][4]=2.99;
+
+        prices[2][0]=8.99;
+        prices[2][1]=7.99;
+        prices[2][2]=9.49;
+        prices[2][3]=9.99;
+        prices[2][4]=10.49;
+
+
+        for(int i=0;i<prices.length;i++){
+            for(int n=0;n<5;n++){
+                System.out.println(prices[i][n]);
+            }
+        }
     }
 }
---
+++ SectionSix/TDArrays.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class TDArrays {
+
+    public static void main(String args[]){
+
+        int[][] grades={
+            {72,74,78,76},{67,67,68,69},{95,96,94,97}
+        };
+       
+
+        //we can print out the array using
+        System.out.println("\t Harry:"+Arrays.toString(grades[0]));
+        System.out.println("\t Ron:"+Arrays.toString(grades[1]));
+        System.out.println("\t Hermione:"+Arrays.toString(grades[2]));
+
+
+
+    }
+    
+}
