1b938491daaa760886c3f1ad485c500bc5b2b6d8
kdhillondev <karanvirdhillon499+developer@Gmail.com>
2023-11-01 20:43:58 -0700

    capstone project

--- Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/Capstone Project/hangman/Hangman.java
+++ Java-Bootcamp-Resources-main/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/Capstone Project/hangman/Hangman.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Hangman {
 
     public static String[] words = {"ant", "baboon", "badger", "bat", "bear", "beaver", "camel",
@@ -68,11 +71,117 @@ public class Hangman {
 
     public static void main(String[] args) {
 
+        System.out.println("Welcome to hangman");
+
+        int incorrectGuessCount=0;
+
+
+        //pick random word
+        String wordChoice=randomWord();
+       
+        //create an array of empty spaces
+        String[] guessSpace= new String[wordChoice.length()];
+        for(int i=0;i<guessSpace.length;i++){
+            guessSpace[i]="_";
+        }
+
+      
+
+        //loop from here check for lives
+
+        while(incorrectGuessCount<7){
+                    System.out.println(gallows[incorrectGuessCount]);
+
+
+              for(int i=0;i<guessSpace.length;i++){
+            //print spaces to screen
+            System.out.print(guessSpace[i]+" ");
+        }
+        System.out.println("\n");
+
+
+        Scanner scan= new Scanner(System.in);
+
+        System.out.print("Take a guess at a letter:");
+        String userGuess=scan.nextLine();
+
+        String[] results=roundOfGame(userGuess,wordChoice);
+
+        if(results[0]=="correct"){
+            //if correct we update the letters in guessSpace, then display it on refresh
+           for(int i=0;i<wordChoice.length();i++){
+
+            char[] charArray=wordChoice.toCharArray();
+           
+            if(results[1].charAt(0)==charArray[i]){
+                guessSpace[i]=results[1];
+             //   System.out.print(guessSpace[i]);
+            }
+      
+           }
+           System.out.println("\n");
+
+           //check if user won
+            boolean gameOver=checkWin(wordChoice, guessSpace);
+            if(gameOver){
+              System.out.println(Arrays.toString(guessSpace));
+                System.out.println("Congrats you win");
+                scan.close();
+
+                break;
+           
+
+        }
+           
+    }else{
+        //update counter, image, and display on refresh
+        incorrectGuessCount++;
+    }
+
+
+        }
+
+if(incorrectGuessCount==7){
+    System.out.println("you loose");
+}
 
 
 
 
+    }
 
+    public static boolean checkWin(String wordChoice, String[] guessSpace){
+
+        boolean gameWin=false;
+        StringBuilder builder = new StringBuilder();
+        for(String s : guessSpace) {
+            builder.append(s);
+        }
+        String str = builder.toString();        
+        System.out.println(wordChoice);
+        if(str.equals(wordChoice)) gameWin=true;
+
+        return gameWin;
+    }
+
+    public static String randomWord(){
+    //THis function returns a random word from the array.
+        int randomNum=(int) (Math.random()*25)+1;
+        String word=words[randomNum];
+        return word;
+    }
+
+    public static String[] roundOfGame(String userInput,String wordChoice){
+//create a function that checks the users guess with the letters in the word
+String[]results={"none","none"};
+for(int i=0;i<wordChoice.length();i++){
+    char c=wordChoice.charAt(i);
+    if(c==userInput.charAt(0)){
+        results[0]="correct";
+        results[1]=String.valueOf(c);
+    }
+}
+   return results;     
     }
 
 }
